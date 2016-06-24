521d00b2897d5f56a2107c47db318cabf3e8078a
Amir <amir_rajabifar@yahoo.com.au>
2016-06-24 13:31:32 +0800

    Operators

--- 08-Operators/jscode.js
+++ 08-Operators/jscode.js
@@ -23,7 +23,7 @@ var number2 = 4;
 
 number1 += number2;
 number1++;
-//The multiplication and division oerators take precedence over
+//The multiplication and division operators take precedence over
 //Subtraction and addition operators
 var number3 = 4 + 3 * 5; 
 document.write(number1 + " ");
