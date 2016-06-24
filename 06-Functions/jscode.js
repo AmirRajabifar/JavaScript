1ec5f5b6814e38660d9a17aaa282c5c816e80019
Amir <amir_rajabifar@yahoo.com.au>
2016-06-24 12:58:51 +0800

    Pass vars to functions

--- 06-Functions/jscode.js
+++ 06-Functions/jscode.js
@@ -1,7 +1,13 @@
-saySomthing(); //call to function
+//saySomthing(); //call to function
+addSomething (455, " Programs to write");
 
-
+/* =========== Functions =========== */ 
 function saySomthing() {
 	alert("This is a function");
 	//all statments go inside curly braces
+}
+
+function addSomething (num, str) {
+	var add = num + str;
+	alert (add);
 }
