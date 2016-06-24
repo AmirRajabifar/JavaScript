77a0bdef9c31e01e5e3b34e6e23b2f4cd8f9e95f
Amir <amir_rajabifar@yahoo.com.au>
2016-06-24 23:16:07 +0800

    Arrays

---
+++ 17-Arrays/jscode.js
@@ -0,0 +1,16 @@
+//array variable
+var roads = ["stone", "dirt", "cement", "tar"];
+
+roads[1] = "cobblestone"
+var roadTravlled = roads[1];
+
+document.write(roadTravlled);
+
+/*
+Another way of writing arrays
+	var raods = [];
+	roads[0] = "stone";
+	roads[1] = "dirt";
+	roads[2] = "cement";
+	roads[3] = "tar";
+*/
