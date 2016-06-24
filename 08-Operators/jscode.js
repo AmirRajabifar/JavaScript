0a6ef4fc6750b4f7a56ff2ab158188267bf76123
Amir <amir_rajabifar@yahoo.com.au>
2016-06-24 13:21:21 +0800

    Operators

---
+++ 08-Operators/jscode.js
@@ -0,0 +1,30 @@
+//Arithmatic and Assignment Operators
+/*
+	=	Equal
+	+	Add
+	-	Subtract
+	*	Multiply
+	/	Division
+
+	Compund operator:
+	+=
+	-=
+	*=
+	/=
+
+	++	Incremental operator
+	--	Decremental operator
+
+*/
+var number1 = 7; //assign 7 tovariable number1
+var number2 = 4; 
+
+//number1 = number1 + number2;
+
+number1 += number2;
+number1++;
+//The multiplication and division oerators take precedence over
+//Subtraction and addition operators
+var number3 = 4 + 3 * 5; 
+document.write(number1 + " ");
+document.write(number3);
