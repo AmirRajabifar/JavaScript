e6bae7904375c471f973ac6f4125c0b39981763b
Amir <amir_rajabifar@yahoo.com.au>
2016-06-25 00:10:49 +0800

    String object in js

---
+++ 22-String Objet/jscode.js
@@ -0,0 +1,35 @@
+/*
+	1. Objecs that JavaScript provides that we can use
+	2. Already written for us so we can just use their properties
+	and methods 
+*/
+var hello = " Hello, how are you doing? ";
+
+
+document.write(hello);
+
+hello = hello.toUpperCase();
+document.write(hello);
+hello = hello.toLowerCase();
+document.write(hello);
+hello = hello.replace("doing", "today");
+document.write(hello);
+hello = hello.bold();
+document.write(hello);
+hello = hello.italics();
+document.write(hello);
+hello = hello.length;
+document.write(hello);
+hello = hello.charAt(4);
+document.write(hello);
+//string object in JavaScript
+/*
+	.toUpperCase()
+	.toLowerCase()
+	.length
+	.charAt([index])
+	.replace('old word', 'new word')
+	.bold()
+	.italics()
+*/
+
